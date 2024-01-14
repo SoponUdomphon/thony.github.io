6193feb168d8f278172d4d06c9d6c720827a406e
mr.sopon <mr.sopon.udo@gmail.com>
2024-01-14 09:05:23 +0700

    final prototype - portfolio web project

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function myFunction() {
     var x = document.getElementById("hidden");
+    var y = document.querySelector('.btn-info');
     if (x.style.display === "none") {
       x.style.display = "block";
+      y.textContent = "No more";
     } else {
       x.style.display = "none";
+      y.textContent = "Know more";
     }
   }
